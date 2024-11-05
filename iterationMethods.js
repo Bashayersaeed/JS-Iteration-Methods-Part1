0e3d1fc970ae3f1d64c09bbaa2131802a06f3ee6
Bashayersaeed <bsaeed83@ymail.com>
2024-11-05 20:28:28 +0300

    Thank you for the feed back, corrections done

--- iterationMethods.js
+++ iterationMethods.js
@@ -1,67 +1,67 @@
 const numbers = [10, 13, 20, 25, 38, 35, 40];
 
-// TASK 1 - Filtering
-// 1) Array >= 25
+// // TASK 1 - Filtering
+// // 1) Array >= 25
 
 const GreaterThanTwnityFive = numbers.filter(function (num){
     return num >= 25; 
 });
-// console.log(GreaterThanTwnityFive);
+    // console.log(GreaterThanTwnityFive);
 
-//2) Array % 5
-GreaterThanTwnityFive = numbers.filter(function (num){
-    return num % 5;
+// //2) Array % 5
+DivisibleByFive = numbers.filter(function (num){
+    return num % 5 === 0;
 }); 
-// console.log(GreaterThanTwnityFive);
+    // console.log(DivisibleByFive);
 
-//TASK 2
-// 3) Each number Sqaured
+// //TASK 2
+// // 3) Each number Sqaured
 squares = numbers.map ((number) =>  number * number);
 
-// console.log(squares);
+// // console.log(squares);
 
-//4) * 2
+// //4) * 2
 TimesTwo = numbers.map(function(number){
     return number * 2;
 })
 // console.log (TimesTwo);
 
-//Task 3
-//5) A- Filter >=20
-//   B- Map square each
+// //Task 3
+// //5) A- Filter >=20
+// //   B- Map square each
 
 const GreaterThanTwinty = numbers.filter(function (num){
     return num >= 20;
 });
-// console.log (GreaterThanTwinty);
+// // console.log (GreaterThanTwinty);
 
 square = GreaterThanTwinty.map(function(number){
     return number * number;
 })
-// console.log (square);
+// // console.log (square);
 
-// 6) A- filter % 5
-//    B- * 3
+// // 6) A- filter % 5
+// //    B- * 3
 
 DivisibleByFive = numbers.filter(function(num){
     return num % 5;
 });
-// console.log(DivisibleByFive);
+// // console.log(DivisibleByFive);
 
 multipleByThree = DivisibleByFive.map(function(num){
     return num * 3;
 });
-// console.log(multipleByThree);
+// // console.log(multipleByThree);
 
-//Challenge
-// logger(array)
-// *
-// * - Accepts an array
-// * - Logs every element of the array
+// //Challenge
+// // logger(array)
+// // *
+// // * - Accepts an array
+// // * - Logs every element of the array
 
-const logger = function (array) {
-    array.forEach(function(element){
-        console.log(element);
-    });
-}
-logger([32,33,,34]);
+// const logger = function (array) {
+//     array.forEach(function(element){
+//         console.log(element);
+//     });
+// }
+// logger([32,33,,34]);
